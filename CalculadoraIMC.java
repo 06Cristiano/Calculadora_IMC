f05cf9e49fd7a7949778b54d606f18785f3cf86d
Cristiano <cristianojw024@gmail.com>
2024-06-06 19:39:43 -0300

    first commit

---
+++ CalculadoraIMC.java
@@ -0,0 +1,41 @@
+public class CalculadoraIMC {
+
+
+public static void main (String[] args) {
+
+    double alturaCentimetro;
+    double peso;
+
+    alturaCentimetro = 1.75;
+    peso = 85.0;
+
+    double totalPeso = alturaCentimetro*alturaCentimetro;
+    double total = peso/totalPeso;
+
+    
+    if (total <= 15.9) {
+    System.out.println("Seu IMC é: "+ total+" apresenta magreza grau III.");
+    }
+    else if  (total <= 16.9){
+        System.out.println("Seu IMC é: "+ total +" apresenta magreza grau II.");
+    }
+    else if (total <= 18.4){
+        System.out.println("Seu IMC é: "+ total +" apresenta magreza grau I.");
+    }
+    else if (total <= 24.9){
+        System.out.println("Seu IMC é: "+ total +" peso ideal!.");
+    }
+    else if (total <= 29.9){
+        System.out.println("Seu IMC é: "+ total +" apresenta sobrepeso.");
+    }
+    else if (total <= 34.9){
+        System.out.println("Seu IMC é: "+ total +" apresenta obesidade grau I.");
+    }
+    else if (total <= 39.9){
+        System.out.println("Seu IMC é: "+ total +" apresenta obesidade grau II ou severa.");
+    }
+    else if (total >= 40){
+        System.out.println("Seu IMC é: "+ total +" apresenta obesidade grau III ou mórbida.");
+    }
+}
+}
